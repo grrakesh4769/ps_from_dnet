42200acfc17f3e79fad94d1a91dd0425c0f3cb63
grrakesh4769 <grrakesh4769@gmail.com>
2019-06-14 19:03:56 +0530

    init vs

---
+++ ps_from_dnet/ps_from_dnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ps_from_dnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
